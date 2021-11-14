29361ae460283fe58bd315a1ccf53b203f038e0d
danilov.fedor <danilov.fedor@otr.ru>
2021-11-14 06:13:20 +0300

    Преобразование эллипса в Полигон. Перенос методов в абстрактный класс

--- src/main/java/ru/projectfx/models/Figure.java
+++ src/main/java/ru/projectfx/models/Figure.java
@@ -9,7 +9,7 @@ import java.util.List;
  * @author Fedor Danilov 13.11.2021
  */
 public abstract class Figure implements CalculateInterface {
-    double area;    //Площадь
+    double area = 0;    //Площадь
     List<Coord> coordinates = new ArrayList<>(); //Координаты
     String type;
 
@@ -20,10 +20,117 @@ public abstract class Figure implements CalculateInterface {
         double x = 0;
         double y = 0;
         for (Coord item: coordinates) {
-            x = item.getX();
-            y = item.getY();
+            x += item.getX();
+            y += item.getY();
         }
-        int listSize = coordinates.size()-1;
+        int listSize = (type.equals("Region")) ? coordinates.size()-1 : coordinates.size();
         return new Coord((x/listSize), (y/listSize));
+    }
+
+    @Override
+    public void rotateFigure(double degree) {
+        double rad = Math.toRadians(degree);
+        double x = 0;
+        double y = 0;
+        double x1 = 0;
+        double y1 = 0;
+        Coord center = barycenter(this.coordinates);
+        List<Coord> newCoord = new ArrayList<>();
+
+        for (Coord item: this.coordinates) {
+            /*Расчет должен происходить относительно начала координат*/
+            x = item.getX() - center.getX();
+            y = item.getY() - center.getY();
+
+            /*Матрица поворота в двумерном пространстве*/
+            x1 = x * Math.cos(rad) - y * Math.sin(rad);
+            y1 = x * Math.sin(rad) + y * Math.cos(rad);
+
+            /*Возвращаем координату на свое место*/
+            x = x1 + center.getX();
+            y = y1 + center.getY();
+
+            newCoord.add(new Coord(x, y));
+        }
+
+        this.coordinates = newCoord;
+    }
+
+    @Override
+    public void moveFigure(double x, double y) {
+        double x1 = 0;
+        double y1 = 0;
+        List<Coord> newCoord = new ArrayList<>();
+        for (Coord item: this.coordinates) {
+            x1 = item.getX() + x;
+            y1 = item.getY() + y;
+            newCoord.add(new Coord(x1, y1));
+        }
+        this.coordinates = newCoord;
+    }
+
+    /*Подсчет происходит с помощью формулы площади Гаусса*/
+    @Override
+    public void calculateArea() {
+        if(!this.type.equals("Region")) return;
+        double area = 0;
+        double x1 = 0;
+        double y1 = 0;
+        double x2 = 0;
+        double y2 = 0;
+        boolean first = true;
+        for (Coord item: this.coordinates) {
+            if (first) {
+                x1 = item.getX();
+                y1 = item.getY();
+                first = false;      /*Заполнение первой координаты в переменные*/
+            } else {
+                x2 = item.getX();
+                y2 = item.getY();
+                area += x1 * y2 - x2 * y1;
+                x1 = x2;
+                y1 = y2;
+            }
+        }
+        this.area = area/2;
+    }
+
+    @Override
+    public void multiplyFigure(double multiply) {
+        Coord center = barycenter(this.coordinates);
+        double x = 0;
+        double y = 0;
+        List<Coord> newCoord = new ArrayList<>();
+        for (Coord item: this.coordinates) {
+            x = multiply * (item.getX() - center.getX()) + center.getX();
+            y = multiply * (item.getY() - center.getY()) + center.getY();
+
+            newCoord.add(new Coord(x, y));
+        }
+        this.coordinates = newCoord;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public List<Coord> getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(List<Coord> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
