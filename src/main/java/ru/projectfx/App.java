57e6701055e3cfff6ec426bf6f7d81e7eab52f93
danilov.fedor <danilov.fedor@otr.ru>
2021-11-19 01:04:46 +0300

    Переименование приложения

--- src/main/java/ru/projectfx/App.java
+++ src/main/java/ru/projectfx/App.java
@@ -11,8 +11,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
+        stage.setTitle("Java FX Application");
         stage.setScene(scene);
         stage.show();
     }
